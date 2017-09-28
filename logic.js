36b0e057dcfc48217c0ec018862c742d7e9f2464
Nicola Limbach <limbach.nicola@gmail.com>
2017-09-28 18:25:37 -0400

    Basic HTML

--- logic.js
+++ logic.js
@@ -1,8 +1,19 @@
 
 $("#letsGo").on("click", function(event) {
-    var city = $("#city-input").val().trim();
-    var month = $("#month-input").val().trim();
+    event.preventDefault();
+    window.location.replace("home.html");
 
-    console.log(city);
+    var city = $("#city-input").val().trim();
+    var month= $("#month-input").val().trim();
+
+
 
 })
+
+var city = localStorage.getItem("city");
+var month = localStorage.getItem("month");
+
+console.log(city);
+console.log(month);
+
+$("#putCity").text(city);
